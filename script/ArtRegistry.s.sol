a099ee8df97f3b9cba383696b5018869b38d94a5
Jorge Tome Blanco <jorge.tome@codere.com>
2026-08-19 12:57:26 +0200

    refactor: Remove default Counter

--- script/ArtRegistry.s.sol
+++ script/ArtRegistry.s.sol
@@ -2,17 +2,19 @@
 pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {ArtRegistry} from "../src/ArtRegistry.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    ArtRegistry public artRegistry;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        address timelockController = address(1);
+        address fundTreasuryProxy = address(2);
+        artRegistry = new ArtRegistry(timelockController, fundTreasuryProxy);
 
         vm.stopBroadcast();
     }
